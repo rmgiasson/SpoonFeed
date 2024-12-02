1baff3874ea068a2b5535274c056e2bbddc3eea9
uhussain11 <uhussain@ufl.edu>
2024-12-02 18:24:10 -0500

    my stuff

--- server/recipe.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const recipeSchema = new mongoose.Schema({
-  title: String,
-  description: String,
-  image: String, // URL for the recipe image
-});
-
-module.exports = mongoose.model('Recipe', recipeSchema);
