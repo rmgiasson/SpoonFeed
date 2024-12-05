b1b71c34632fa012285dbc04ffd6d7e9bebed4ab
dimitridim8 <116752261+dimitridim8@users.noreply.github.com>
2024-12-04 23:47:49 -0500

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import Register from './Register';
 import Login from './Login';
 import LogMeal from './LogMeal'; // Import the LogMeal component
 import './App.css';
+import MealLog from './MealLog';
 
 function App() {
   const location = useLocation(); // Hook to get the current location
@@ -14,20 +15,21 @@ function App() {
       <header className="app-header">
         <h1 className="app-heading">Welcome to SpoonFeed</h1>
         <nav className="nav-links">
-          {location.pathname === '/log-meal' ? (
-            <Link to="/">Home</Link> // Show Home link when on /log-meal
+          {location.pathname === '/register' || location.pathname === '/login' || location.pathname === '/log-meal' ? (
+            <Link to="/" className="home-button">Home</Link> // Show Home link when on /log-meal
           ) : (
-            <Link to="/log-meal">Log Meal</Link> // Show Log Meal link otherwise
+            <Link to="/log-meal" className="log-meal-button">Log Meal</Link> // Show Log Meal link otherwise
           )}
         </nav>
       </header>
       <main>
-        <Routes>
-          <Route path="/" element={<RecipeDisplay />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/log-meal" element={<LogMeal />} /> {/* Added LogMeal route */}
-        </Routes>
+      <Routes>
+        <Route path="/" element={<RecipeDisplay />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/log-meal" element={<LogMeal />} />
+        <Route path="/meal-logs" element={<MealLog />} />
+      </Routes>
       </main>
     </div>
   );
